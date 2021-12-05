e852b3e11f64a73459faa77b42c7a7baf4c23b36
Elcilene <elcilenematosaragao@gmail.com>
2021-12-05 13:36:57 -0300

    add start

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const exress = require("express")
 const app = express()
 
-app.get("/", function(res,req)){
+app.get("/", function(req,res)){
     res.send("WORKING!")
 }
 app.listenprocess.env.PORTA || (8000)
